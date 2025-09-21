599d8664f8e41d3d0b0004b43418c1ec773fb538
rizaul0 <rizaulsheikh0@gmail.com>
2025-09-21 12:20:08 +0530

    second commit

--- day3/counter/src/App.jsx
+++ day3/counter/src/App.jsx
@@ -1,37 +1,35 @@
 import { useState } from "react";
 
 function App() {
-  let [counter,setcounter]=useState(15)
+  let [counter, setcounter] = useState(15)
   // let counter =15
-  const addValue = () =>{
-  if (counter>=20) {
-    setcounter(20)
+  const addValue = () => {
+    if (counter >= 20) {
+      setcounter(20)
+    }
+    else {
+      setcounter(counter + 1)
+
+    }
+
   }
-  else
-  {
-  setcounter(counter+1)
-  
+  const remove = () => {
+    if (counter <= 0) {
+      setcounter(0)
+    }
+    else {
+      setcounter(counter - 1)
+
+    }
   }
-  
- }
- const remove = () =>{
-  if (counter<=0) {
-    setcounter(0)
-  }
-  else
-  {
-  setcounter(counter-1)
-  
-  }
- }
 
   return (
     <>
-    <h1>simple counter</h1>
-    <h2>counter value: {counter}</h2>
-    <button onClick={addValue}>add value  </button>
-    <br />
-    <button onClick={remove}>remove value</button>
+      <h1>simple counter</h1>
+      <h2>counter value: {counter}</h2>
+      <button onClick={addValue}>add value  </button>
+      <br />
+      <button onClick={remove}>remove value</button>
     </>
   )
 }
