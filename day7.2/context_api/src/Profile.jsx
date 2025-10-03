5147db5cac6c9cab45dd3ff7d5e1b6297b875bb3
rizaul0 <rizaulsheikh0@gmail.com>
2025-10-03 19:17:06 +0530

    day7 commit

---
+++ day7.2/context_api/src/Profile.jsx
@@ -0,0 +1,11 @@
+import React,{useContext} from 'react'
+import Usercontext from './context/Usercontext'
+
+function Profile() {
+  const {user} = useContext(Usercontext)
+  if (!user) return <div>Please Login</div>
+  
+  return <div>welcome {user.username}</div>
+}
+
+export default Profile
