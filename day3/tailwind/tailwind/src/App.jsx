9c6cd62c9fd044284659430dbc873a48b8ad3582
rizaul0 <rizaulsheikh0@gmail.com>
2025-10-04 13:15:04 +0530

    day8

--- day3/tailwind/tailwind/src/App.jsx
+++ day3/tailwind/tailwind/src/App.jsx
@@ -17,10 +17,6 @@ function App() {
   }
 
   
-
-  
-
-
   return (
     <>
      <h1 className='bg-green-500'>simple counter </h1>
--- day7.2/context_api/src/context/Contextprovider.jsx
+++ day7.2/context_api/src/context/Contextprovider.jsx
@@ -10,6 +10,6 @@ return(
     </Usercontext.Provider>
     </>
 )
-}
+} 
 
 export default Contextprovider
